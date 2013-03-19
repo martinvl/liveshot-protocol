99513ebcbdd34188f76ca33f5da4c57c9a7294ab
Martin Vonheim Larsen <martinvl@student.matnat.uio.no>
2013-03-19 19:59:07 +0100

    Fixed linking error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
-    CardBuilder:require('src/CardBuilder'),
-    ConfigBuilder:require('src/ConfigBuilder'),
-    ResultBuilder:require('src/ResultBuilder'),
-    ShooterBuilder:require('src/ShooterBuilder'),
-    ShotBuilder:require('src/ShotBuilder')
+    CardBuilder:require('./src/CardBuilder'),
+    ConfigBuilder:require('./src/ConfigBuilder'),
+    ResultBuilder:require('./src/ResultBuilder'),
+    ShooterBuilder:require('./src/ShooterBuilder'),
+    ShotBuilder:require('./src/ShotBuilder')
 };
