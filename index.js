2ff793f15a00246cfa348ceaf57f60ae7452b5e4
Martin Vonheim Larsen <martinvl@student.matnat.uio.no>
2013-03-20 11:57:40 +0100

    Restructured package contents

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 module.exports = {
-    card:{
-        CardBuilder:require('./src/card/CardBuilder'),
-        ConfigBuilder:require('./src/card/ConfigBuilder'),
-        ResultBuilder:require('./src/card/ResultBuilder'),
-        ShooterBuilder:require('./src/card/ShooterBuilder'),
-        ShotBuilder:require('./src/card/ShotBuilder')
-    },
-    target:{
-        TargetBuilder:require('./src/target/TargetBuilder'),
-        RingTargetBuilder:require('./src/target/RingTargetBuilder')
-    }
+    CardBuilder:require('./src/card/CardBuilder'),
+    ConfigBuilder:require('./src/card/ConfigBuilder'),
+    ResultBuilder:require('./src/card/ResultBuilder'),
+    ShooterBuilder:require('./src/card/ShooterBuilder'),
+    ShotBuilder:require('./src/card/ShotBuilder')
 };
